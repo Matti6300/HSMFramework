402b5dad8c13c06b98baa6d91c8675837facf472
bharath8858 <105233330+bharath8858@users.noreply.github.com>
2024-07-31 19:16:20 +0530

    new class added

---
+++ GUISeleniumFramework/src/test/java/com/comcast/hsm/AdminFunctional/NewClass.java
@@ -0,0 +1,11 @@
+package com.comcast.hsm.AdminFunctional;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubu
+		System.out.println("bharath");
+
+	}
+
+}
